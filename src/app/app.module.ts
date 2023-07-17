5833f96f2ad076d6221e06444901b37ab913c009
Hudson <hcarpenter1@avidxchange.com>
2023-07-17 16:36:08 -0400

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CfmComponent } from './cfm.component'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CfmComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cfm.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cfm',
+    template: '<h2>Cfm</h2>'
+})
+export class CfmComponent {
+
+}
